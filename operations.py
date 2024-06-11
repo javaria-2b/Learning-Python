5bc36b5deeac51d7db9036d608d546c8cbcaf18a
mr.junaidshaukat@gmail.com <mr.junaidshaukat@gmail.com>
2024-06-11 23:07:25 +0500

    basic calculations

---
+++ operations.py
@@ -0,0 +1,17 @@
+# addition
+a = 5
+b = 17
+sum = a + b
+print(sum)
+
+#subtraction
+diff = a - b
+print(diff)
+
+#division
+divide = a / b
+print(divide)
+
+#multiplication
+multiply = a * b
+print(multiply)
